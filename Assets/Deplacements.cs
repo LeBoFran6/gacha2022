eb46c1634a8191d041f52bbe874fb5938843bcd8
Benjamin Villa <ywqqqq@gmail.com>
2022-11-27 11:20:58 +0100

    Jump and gyro features (very stable)

--- Assets/Deplacements.cs
+++ Assets/Deplacements.cs
@@ -5,29 +5,74 @@ using UnityEngine;
 public class Deplacements : MonoBehaviour
 {
     public Rigidbody rb;
-    bool G = false;
-    bool D = false;
-    int positionPlayer = 1;
+    public bool G = false;
+    public bool D = false;
+    public int positionPlayer = 1;
+
+    private Vector2 startTouchPosition;
+    private Vector2 endTouchPosition;
+
+    [SerializeField] private Vector3 startpos;
+    [SerializeField] private Vector3 endpos;
+    [SerializeField] private AnimationCurve curve;
+    [SerializeField] private float percentage;
+    private float dir = 1;
+
+    private bool isJumping = false;
+
     public void Gauche()
     {
-        Debug.Log("G");
         G = true;
     }
     public void Droite()
     {
-        Debug.Log("D");
         D = true;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-       
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if(positionPlayer != 0)
+    { 
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTouchPosition = Input.GetTouch(0).position;
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            endTouchPosition = Input.GetTouch(0).position;
+
+            // Swipe vers le bas
+            if (endTouchPosition.y < startTouchPosition.y)
+            {
+                
+            }
+
+            // Swipe vers le haut
+            if (endTouchPosition.y > startTouchPosition.y)
+            {
+                isJumping = true;
+
+                startpos = transform.position;
+                endpos = transform.position + new Vector3(0, 3, 0);
+            }
+        }
+
+        if (isJumping)
+        {
+            transform.position = Vector3.Lerp(startpos, endpos, curve.Evaluate(percentage));
+
+            percentage += Time.deltaTime * dir;
+
+            if (Time.deltaTime * dir > 1.0f || percentage < 0) { dir = -dir; }
+
+            if (percentage > 1)
+            {
+                isJumping = false;
+                percentage = 0;
+            }
+        }
+
+        if (positionPlayer != 0)
         {
             if (G)
             {
---
+++ Assets/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject gameOverTextObject;
+
+    private bool isAlive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isAlive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isAlive)
+        {
+            gameOverTextObject.SetActive(true);
+            player.SetActive(false);
+        }
+    }
+
+    public void deathEvent()
+    {
+        isAlive = false;
+    }
+}
